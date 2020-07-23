4cd4179acebd6cb2becb3fef87621bdefe8e3ce3
Kit Chan <kichan@apache.org>
2020-07-22 23:15:15 -0700

    Changing parameter name for docker login

--- build-master.sh
+++ build-master.sh
@@ -5,6 +5,6 @@ REPONAME="ats-ingress"
 TAG="latest"
 TARGET=atsingress/${REPONAME}:${TAG}
 
-echo "${DOCKERHUB_TOKEN}" | docker login -u "${DOCKERHUB_USER}" --password-stdin
+echo "${DOCKERHUB_TOKEN}" | docker login --username "${DOCKERHUB_USER}" --password-stdin
 docker build -t ${TARGET} .
 docker push ${TARGET}
